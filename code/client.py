0c82a8a8187b592e641ef59b48d3a6abd220a4d7
Diego Lòpez <None>
2017-11-28 15:31:49 +0100

    Client makes the given number of request to the server

--- code/client.py
+++ code/client.py
@@ -3,7 +3,7 @@ import time
 
 
 def get(path, port):
-
+    """ Just makes a GET request and prints the header """
 
     request = 'GET %s HTTP/1.0\r\n\r\n' % path
     connection_path = ('localhost', port)
@@ -20,3 +20,18 @@ def get(path, port):
             body = (b''.join(some_chunks)).decode()
             print(body.split('\n')[0])
             return 1
+
+def run(request_to_do):
+    """ Takes a number and makes so many request"""
+
+    path = '/'
+    port = 5000
+
+    request_number = 0 
+    while request_to_do:
+        print('request number ' + str(request_number))
+        get(path, port)
+        print('---------------\n')
+
+        request_number += 1
+        request_to_do -= 1 
